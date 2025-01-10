3ef673ca9b3ee6f4e9377c45264389686c2bc0c8
Prasanna <prasanna.venkatesh95@gmail.com>
2025-01-11 00:51:55 +0530

    MaximumSubArray

---
+++ DSA/MaximumSubArray.java
@@ -0,0 +1,29 @@
+package DSA;
+public class MaximumSubArray{
+    static int maxLen(int[]  input){
+        // given a list input
+        // Get the max sum of any part of the array
+        // Time Complexity--> o(n)
+
+        // Intuition
+        // """
+        // Create two variables
+        // currentSum :- Stores sum of array
+        // maxSum :- Keeps track of the maxSum
+        // """ 
+        int currentSum = input[0];
+        int maxSum = input[0];
+        // starting at the same place with the same sum
+        // looping the elements of the list
+        for (int i = 1; i < input.length; i++){
+            currentSum = Math.max(input[i], currentSum + input[i]);
+            maxSum = Math.max(currentSum, maxSum);
+        }
+        return maxSum;
+    }
+    public static void main(String[] args){
+        int[] input={-2,1,-3,4,-1,2,1,-5,4};
+        int result=maxLen(input);
+        System.out.println(result);
+    }
+}
